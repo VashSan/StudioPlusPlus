2e5cf597ca247092c6ff361056983bbcfa76560d
VashSan <alex@palc.de>
2013-08-15 20:49:18 +0200

    added message handler for debugging

--- Studio/main.cpp
+++ Studio/main.cpp
@@ -1,9 +1,48 @@
 #include "mainwindow.h"
+#include <QDebug>
 #include <QApplication>
+#include <QFile>
+#include <QTime>
+
+
+void myMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+	Q_UNUSED(context);
+
+	QString time = QTime::currentTime().toString("hh:mm:ss.zzz");
+
+	QString txt;
+	switch (type)
+	{
+	case QtDebugMsg:
+		txt = QString("%1 Debug: %2").arg(time, msg);
+		break;
+
+	case QtWarningMsg:
+		txt = QString("%1 Warning: %2").arg(time, msg);
+		break;
+
+	case QtCriticalMsg:
+		txt = QString("%1 Critical: %2").arg(time, msg);
+		break;
+
+	case QtFatalMsg:
+		txt = QString("%1 Fatal: %2").arg(time, msg);
+		abort();
+	}
+
+	QFile outFile("Studio.log");
+	outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+	QTextStream ts(&outFile);
+	ts << txt << endl;
+}
 
 int main(int argc, char *argv[])
 {
+	// TODO install message handler only on certain command line param
+	qInstallMessageHandler(myMessageHandler);
 	QApplication a(argc, argv);
+
 	MainWindow w;
 	w.show();
 	
